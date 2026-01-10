330d0387344b90fc5acdad0309591f739743c912
yu-9814 <yu0217lvfv@gmail.com>
2026-01-10 18:53:26 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 7;
+
+console.log(num);
+
+if (num % 3 == 0) {
+	console.log('3の倍数です');
+}
+
+if (num % 5 == 0) {
+	console.log('5の倍数です');
+}
+
+if (num % 3 == 0 && num % 5 == 0) {
+	console.log('3と5の倍数です');
+}
