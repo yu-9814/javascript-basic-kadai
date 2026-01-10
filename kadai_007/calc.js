4373d1d08db0c2cf467d0aa98da41c128326ff9c
yu-9814 <yu0217lvfv@gmail.com>
2026-01-11 00:49:39 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -6,10 +6,10 @@ if (num % 3 == 0) {
 	console.log('3の倍数です');
 }
 
-if (num % 5 == 0) {
+if else (num % 5 == 0) {
 	console.log('5の倍数です');
 }
 
-if (num % 3 == 0 && num % 5 == 0) {
+if else (num % 3 == 0 && num % 5 == 0) {
 	console.log('3と5の倍数です');
 }
