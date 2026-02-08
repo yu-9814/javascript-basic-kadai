d733257cf17785562228adf7aa4b1ad752200693
yu-9814 <yu0217lvfv@gmail.com>
2026-02-08 14:54:33 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date ();
+
+const date1 = today.getFullYear() + '年';
+const date2 = today.getMonth() + 1 + '月';
+const date3 = today.getDate()+ '日';
+
+console.log(date1,date2,date3)
