f305231829ae97355376c70d30025cbad7e14b59
yu-9814 <yu0217lvfv@gmail.com>
2026-03-28 12:22:04 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const Bun = document.getElementById('btn');
+
+Bun.addEventListener('click' , () => {
+
+  const h2 = document.getElementById('text');
+
+	h2.textContent = 'ボタンがクリックされました';
+	
+	});
+
